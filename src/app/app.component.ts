13e739e7a38d99ec0a2428d1df3db67e4298c3a9
2030lab <bob.score@2030lab.com>
2022-05-25 16:11:29 +0100

    Completed Task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  detectTyping(event: any) {
+    this.enteredText = (event.target as HTMLInputElement).value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'grey';
+    }
+    return randomLetter === enteredLetter ? 'green' : 'red';
+  }
 }
